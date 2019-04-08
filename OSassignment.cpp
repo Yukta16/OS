8a34f5b2c8525f50df989baf52a9f694cdf6df4a
Yukta16 <35625930+Yukta16@users.noreply.github.com>
2019-04-08 18:43:28 +0530

    Update OSassignment.cpp

--- OSassignment.cpp
+++ OSassignment.cpp
@@ -3,21 +3,22 @@
 #include<pthread.h>
 
 
-
 #define MIN_PID 100
 #define MAX_PID 1000
 
 
 pthread_mutex_t L;
 
-struct process {
+struct process 
+	{
 	int pid,Alloc;
-}arr[MAX_PID-MIN_PID];
+	}arr[MAX_PID-MIN_PID];
 
 int allocate_map() { 
-	int i; 
-	for(i=MIN_PID;i<=MAX_PID;i++) 
+	
+	for(int i=MIN_PID;i<=MAX_PID;i++) 
 	{
+		
 		arr[i-MIN_PID].pid=i;
 		arr[i-MIN_PID].Alloc=0;
 	}
@@ -25,34 +26,53 @@ int allocate_map() {
 }
 
 
+int allocate_pid()    
+{  
+		for(int i=0;i<MAX_PID;i++)
+		{
+				if (arr[i].Alloc==0) 
+				{
+						pthread_mutex_lock(&L);
+						arr[i].Alloc=1;
+						pthread_mutex_unlock(&L); 
+						return arr[i].pid;
+				}
+		}
+	return -1; 
+}
+
+
 void* func(void *i) 
 {
-	printf("A new process is created.\n");
-
+		int pid=allocate_pid();
+	printf("A new process %d is created.\n",pid);
 }
 
-void CreateP() {
-	int i=0;
-	pthread_t a[6]; 
-	if(pthread_mutex_init(&L,NULL)!=0)
+void CreateP() 
 	{
-		printf("Mutex init.");
-	}	
-	while(i<6) {
-		pthread_create(&a[i],NULL,func,NULL);
-		i++;
-	}
 	
-	i=0;
-	while(i<6) {
-		pthread_join(a[i],NULL);
-		i++;
-	}	
-}
+				pthread_t a[6]; 
+	
+				if(pthread_mutex_init(&L,NULL)!=0)
+						{
+							printf("Mutex initialized");
+						}	
+				for(int i=0;i<6;i++) 
+						{
+							pthread_create(&a[i],NULL,func,NULL);
+						}
+	
+	
+				for(int i=0;i<6;i++) 
+						{
+							pthread_join(a[i],NULL);
+		
+						}	
+	}
 
 
-int main() {
-	allocate_map();
+int main() 
+	{
+		allocate_map();
 		CreateP();
 	}
-
